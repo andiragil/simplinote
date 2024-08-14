08b11c0639c8e3f74e1f7abf69930387bb1f0b70
andragil <andiragilw09@gmail.com>
2024-08-15 01:36:18 +0700

    fixing tag

--- app/page.tsx
+++ app/page.tsx
@@ -27,16 +27,16 @@ const Notes = async () => {
   return (
     <ChakraProvider>
       <div className="container mx-auto">
-        <div className="mx-2">
+        <div className="mx-4">
           <h1 className="text-3xl font-bold text-gray-800 my-4 border-b-2 border-teal-500 pb-2">
             Simpli <span className="font-light">Note</span>
           </h1>
-          <p className="text-xl text-gray-600 mb-6 italic">"Stay Organized, Stay Simple"</p>
+          <p className="text-xl text-gray-600 mb-6 italic">&quot;Stay Organized, Stay Simple&quot;</p>
           <div className="flex justify-between items-center mt-4 mb-2">
             <AddNote />
           </div>
         </div>
-        <SimpleGrid spacing={4} className="mx-3" templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}>
+        <SimpleGrid spacing={4} className="mx-5" templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}>
           {notes.map((note) => (
             <Card key={note.id}>
               <CardHeader>
